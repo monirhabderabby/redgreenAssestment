173a56f1c3fd6323cce5f4d8b1038594a74f9c9a
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-09-17 20:40:37 +0600

    added responsive chart

--- src/components/DashboardOverView/TotalSales/TotalSales.js
+++ src/components/DashboardOverView/TotalSales/TotalSales.js
@@ -1,53 +1,54 @@
 import { DatePicker } from "antd";
 import React from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import {
-    CartesianGrid,
-    Line,
-    LineChart,
-    ResponsiveContainer,
-    Tooltip,
-    XAxis,
-    YAxis,
-} from "recharts";
+import { Area, AreaChart, ResponsiveContainer, Tooltip } from "recharts";
 import "../../../App.css";
 
 const TotalSales = () => {
     const data = [
         {
-            name: "0",
-            uv: 1000,
-            pv: 2400,
-            amt: 2400,
+            value: 500,
+            y: "1Jt",
         },
         {
-            name: "07",
-            uv: 1500,
-            pv: 1398,
-            amt: 2210,
+            value: 100,
+            y: "2Jt",
         },
         {
-            name: "14",
-            uv: 500,
-            pv: 9800,
-            amt: 2290,
+            value: 300,
+            y: "3Jt",
         },
         {
-            name: "21",
-            uv: 780,
-            pv: 3908,
-            amt: 2000,
+            value: 250,
+            y: "4Jt",
         },
         {
-            name: "28",
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
+            value: 750,
+            y: "5Jt",
+        },
+        {
+            value: 550,
+            y: "5Jt",
+        },
+    ];
+
+    const product = [
+        {
+            product: "TV",
+            price: "600.000",
+        },
+        {
+            product: "Laptop",
+            price: "1.200.000",
+        },
+        {
+            product: "Other",
+            price: "210.287",
         },
     ];
     return (
         <div className=" md:col-span-8 h-[366px] card__shadow rounded-6px border-[1px] border-[#E3E8EF]">
-            <header className="p-[20px] w-full flex items-center justify-between ">
+            <header className="p-[20px]  w-full flex items-center justify-between ">
                 <p className="font-normal text-[24px] leading-32px text-[#181818] font-popins">
                     Total Sale
                 </p>
@@ -56,62 +57,62 @@ const TotalSales = () => {
                     <BsThreeDotsVertical className="ml-[10px] md:ml-[41px] text-2xl" />
                 </div>
             </header>
-            <section className="">
-                <div className="hidden md:block overflow-auto">
-                    <ResponsiveContainer width="100%" aspect={4}>
-                        <LineChart
-                            width={500}
-                            height={300}
-                            data={data}
-                            margin={{
-                                top: 5,
-                                right: 30,
-                                left: 10,
-                                bottom: 5,
-                            }}
-                        >
-                            <CartesianGrid horizontal="true" vertical="" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
+            <div className="grid grid-cols-12 p-[20px]">
+                <div className="col-span-12 md:col-span-2">
+                    <div className="grid grid-cols-4 md:grid-cols-1 md:gap-y-3">
+                        {product?.map((p) => {
+                            return (
+                                <>
+                                    <div className="flex flex-col">
+                                        <span className=" text-[10px] md:text-[16px] text-[#5C5C5C] font-thin font-popins">
+                                            {p.product}
+                                        </span>
+                                        <span className=" text-[8px] md:text-[20px] font-normal text-[#181818] font-popins">
+                                            {p.price}
+                                        </span>
+                                    </div>
+                                </>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className=" h-[150px] col-span-12 md:col-span-10">
+                    <ResponsiveContainer width="100%" height="100%">
+                        <AreaChart data={data}>
+                            <defs>
+                                <linearGradient
+                                    id="colorView"
+                                    x1="0"
+                                    y1="0"
+                                    x2="0"
+                                    y2="0"
+                                >
+                                    <stop
+                                        offset="10%"
+                                        stopColor="#708FFF"
+                                        stopOpacity={0.4}
+                                    ></stop>
+                                    <stop
+                                        offset="100%"
+                                        stopColor="#000000ff"
+                                        stopOpacity={0.2}
+                                    ></stop>
+                                </linearGradient>
+                            </defs>
                             <Tooltip />
-                            <Line
+                            <Area
                                 type="monotone"
-                                dataKey="uv"
-                                stroke="#2D5BFF"
+                                dataKey="value"
+                                stroke="#708FFF"
                                 strokeWidth={2}
-                                activeDot={{ r: 8 }}
+                                fill="#FFFFFF"
+                                animationBegin={800}
+                                animationDuration={2000}
                             />
-                        </LineChart>
+                        </AreaChart>
                     </ResponsiveContainer>
                 </div>
-                <div className="md:hidden ">
-                    <ResponsiveContainer width="100%" aspect={1}>
-                        <LineChart
-                            width={500}
-                            height={300}
-                            data={data}
-                            margin={{
-                                top: 5,
-                                right: 30,
-                                left: 10,
-                                bottom: 5,
-                            }}
-                        >
-                            <CartesianGrid horizontal="true" vertical="" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Line
-                                type="monotone"
-                                dataKey="uv"
-                                stroke="#2D5BFF"
-                                strokeWidth={2}
-                                activeDot={{ r: 8 }}
-                            />
-                        </LineChart>
-                    </ResponsiveContainer>
-                </div>
-            </section>
+            </div>
         </div>
     );
 };
